94f10d9f907a449983ec4a1f0a51076df79fbdbe
BitterTrue <134939407+BitterTrue@users.noreply.github.com>
2023-06-20 15:24:33 +0300

    Update Program.cs

--- task19/Program.cs
+++ task19/Program.cs
@@ -8,8 +8,6 @@ int Numb = int.Parse(Console.ReadLine());
 int N1 = Numb / 100;
 int N2 = (Numb % 10)*100 + ((Numb % 100)/10)*10 + (Numb % 1000)/100;
 
-Console.WriteLine($"N1= {N1}");
-Console.WriteLine($"N2= {N2}");
 if (N1==N2)
 {
   Console.WriteLine($"число является палиндромом {N1} = {N2}");
